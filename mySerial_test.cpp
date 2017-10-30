7c826ae2fb6e986588d1b1029575feabcc0a8299
Darlan <darlan.jurak@gmail.com>
2017-10-30 17:04:31 -0200

    added inpute parameter (serial)

---
+++ mySerial_test.cpp
@@ -0,0 +1,24 @@
+#include "mySerial.h"
+#include <iostream>
+using namespace std;
+
+
+
+int  main( int argc, char** argv )
+{
+
+    mySerial serial(argv[1],115200);
+
+    // One Byte At the time
+     serial.Send(128);
+     serial.Send(132);
+
+    // An array of byte
+    unsigned char  dataArray[] = { 142,0};
+    serial.Send(dataArray,sizeof(dataArray));
+
+    // Or a string
+    serial.Send("this is it\r\n");
+
+    return 0;
+}
