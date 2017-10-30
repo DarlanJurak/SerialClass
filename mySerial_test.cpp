ebdc73df870dedecf479a8934761416ac2579adf
Darlan <darlan.jurak@gmail.com>
2017-10-30 17:36:22 -0200

    testing

--- mySerial_test.cpp
+++ mySerial_test.cpp
@@ -9,16 +9,22 @@ int  main( int argc, char** argv )
 
     mySerial serial(argv[1],115200);
 
-    // One Byte At the time
-     serial.Send(128);
-     serial.Send(132);
+    while(1){
 
-    // An array of byte
-    unsigned char  dataArray[] = { 142,0};
-    serial.Send(dataArray,sizeof(dataArray));
+        // One Byte At the time
+        serial.Send(128);
+        serial.Send(132);
 
-    // Or a string
-    serial.Send("this is it\r\n");
+        // An array of byte
+        // unsigned char  dataArray[] = { 142,0};
+        // serial.Send(dataArray,sizeof(dataArray));
+
+        // // Or a string
+        // serial.Send("this is it\r\n");
+
+        cout << serial.Receive() << endl;
+
+    }
 
     return 0;
 }
